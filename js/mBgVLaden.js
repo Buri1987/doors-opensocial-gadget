a11fb4dd0eab01869bbbc9482b39325a56b72910
Robert Burmeister <124580891+Buri1987@users.noreply.github.com>
2026-03-03 14:39:15 +0100

    Attribut umbenannt: System-/Baugruppennummer:

--- js/mBgVLaden.js
+++ js/mBgVLaden.js
@@ -2,7 +2,7 @@
 async function mBgVLaden(){
 	if(typeof inhaltMBgV === "undefined" ){
 		let promiseInhaltMBgV = new Promise(function(resolve, reject){                  
-			RM.Data.getContentsStructure(refBgV, ["Baugruppennummer",RM.Data.Attributes.NAME], function(res){
+			RM.Data.getContentsStructure(refBgV, ["System-/Baugruppennummer",RM.Data.Attributes.NAME], function(res){
 				resolve(res.data);
 			});
 		});	
